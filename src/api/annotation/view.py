88594cddaccbe300480387d1730ca0452c16d3b3
Giovani Liskoski Zanini <giovanilzanini@hotmail.com>
2022-02-13 23:12:52 -0300

    Revert "Feat: project documentation"
    This reverts commit 1369a1edbbab97ebf743175199b30332d1594b2d.

--- src/api/annotation/view.py
+++ src/api/annotation/view.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from fastapi import Response, status
+from fastapi import Response
 from fastapi.routing import APIRouter
 from fastapi_pagination import Page
 from typing import Optional
@@ -14,7 +14,7 @@ from .model import Annotation
 
 router = APIRouter(
     prefix="/annotation",
-    tags=["annotation"],   
+    tags=["schedule"],
 )
 annotation = Annotation()
 
@@ -23,22 +23,6 @@ annotation = Annotation()
 async def create(
     body: CreateAndPutAnnotationRequest
 ) -> AnnotationResponse:
-    """
-    ## Description
-    * Create one annotation, that you can add in notebook.
-    
-    ## Body
-    * **title**: Title of your annotation.
-    * **tags**: List of tags, that is possible to group annotations.
-
-    ## Response
-    * **id**: Annotation id
-    * **title**: Title of your annotation.
-    * **creation_date**: Annotation creation data.
-    * **modification_date**: Annotation modification data.
-    * **tags**: List of tags, that is possible to group annotations.
-
-    """
     return await annotation.create(body)
 
 
@@ -48,138 +32,37 @@ async def create(
 )
 async def list(
         response: Response,
-        page: Optional[int] = 1,
-        size: Optional[int] = 1
+        page: Optional[int] = None,
+        size: Optional[int] = None
 ) -> Page[AnnotationResponse]:
-    """
-    ## Description
-    * List all annotations with pagination.
-
-    ## Parameters
-    * **page**: Page that you want to get. Defaults to 1.
-    * **size**: Size of the page that you want to get. Defaults to 1.
-
-    ## Response
-    * Code 200
-        * **items**: List of annostations.
-        * **total**: Total of items.
-        * **page**: Page number.
-        * **size**: Page size.
-    * Code 404
-        * **detail**: Error detail.
-        * **loc**: Specifies which field the error is in.
-        * **msg**: Error message.
-        * **type**: Error type.
-
-    """
     return await annotation.list(response)
 
-@router.get('/{annotation_id}', response_model=AnnotationResponse)
+@router.get('/{annotation_id}')
 async def get(
         annotation_id: str
 ) -> AnnotationResponse:
-    """
-    ## Description
-    * List annotation detail.
-
-    ## Parameters
-    * **annotation_id**: Id of annotation that you need to get.
-
-    ## Response
-    * Code 200
-        * **id**: Annotation id
-        * **title**: Title of your annotation.
-        * **creation_date**: Annotation creation data.
-        * **modification_date**: Annotation modification data.
-        * **tags**: List of tags, that is possible to group annotations.
-    * Code 404
-        * **detail**: Error detail.
-        * **loc**: Specifies which field the error is in.
-        * **msg**: Error message.
-        * **type**: Error type.
-
-    """
     return await annotation.get(annotation_id)
 
 
-@router.put('/{annotation_id}, response_model=AnnotationResponse')
+@router.put('/{annotation_id}')
 async def put(
         annotation_id: str,
         body: CreateAndPutAnnotationRequest
 ) -> AnnotationResponse:
-    """
-    ## Description
-    * Update all annotation fields.
-
-    ## Parameters
-    * **annotation_id**: Id of annotation that you need to put.
-
-    ## Body
-    * **title**: Title of your annotation.
-    * **tags**: List of tags, that is possible to group annotations.
-
-    ## Response
-    * Code 200
-        * **id**: Annotation id
-        * **title**: Title of your annotation.
-        * **creation_date**: Annotation creation data.
-        * **modification_date**: Annotation modification data.
-        * **tags**: List of tags, that is possible to group annotations.
-    * Code 404
-        * **detail**: Error detail.
-        * **loc**: Specifies which field the error is in.
-        * **msg**: Error message.
-        * **type**: Error type.
-    """
     return await annotation.put(body, annotation_id)
 
 
-@router.patch('/{annotation_id}', response_model=AnnotationResponse)
+@router.patch('/{annotation_id}')
 async def patch(
         annotation_id: str,
         body: PatchAnnotationRequest
 ) -> AnnotationResponse:
-    """
-    ## Description
-    * Update annotation with optional fields.
-
-    ## Parameters
-    * **annotation_id**: Id of annotation that you need to patch.
-
-    ## Body
-    * **title**: Title of your annotation.
-    * **tags**: List of tags, that is possible to group annotations.
-
-    ## Response
-    * Code 200
-        * **id**: Annotation id
-        * **title**: Title of your annotation.
-        * **creation_date**: Annotation creation data.
-        * **modification_date**: Annotation modification data.
-        * **tags**: List of tags, that is possible to group annotations.
-    * Code 404
-        * **detail**: Error detail.
-        * **loc**: Specifies which field the error is in.
-        * **msg**: Error message.
-        * **type**: Error type.
-    """
     return await annotation.patch(body, annotation_id)
 
 
-@router.delete(
-    '/{annotation_id}',
-    status_code=status.HTTP_204_NO_CONTENT,
-    response_description="No content"
-)
+@router.delete('/{annotation_id}')
 async def delete(
         annotation_id: str
-):
-    """
-    ## Description
-    * Delete annotation.
-
-    ## Parameters
-    * **annotation_id**: Id of annotation that you need to delete.
-    """
-    await annotation.delete(annotation_id)
+) -> Response:
+    return await annotation.delete(annotation_id)
 
--- src/api/notebook/view.py
+++ src/api/notebook/view.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from fastapi import Response, status
+from fastapi import Response
 from fastapi.routing import APIRouter
 from fastapi_pagination import Page
 from typing import Optional
@@ -14,7 +14,7 @@ from .model import Notebook
 
 router = APIRouter(
     prefix="/notebook",
-    tags=["notebook"],
+    tags=["schedule"],
 )
 notebook = Notebook()
 
@@ -23,27 +23,6 @@ notebook = Notebook()
 async def create(
     body: CreateAndPutNotebookRequest
 ) -> NotebookResponse:
-    """
-    ## Description
-    * Create one notebook, that you can relate with annotation.
-
-    ## Body
-    * **description**: Description of your annotation.
-    * **annotations**: List of annotation ID that will be related.
-
-    ## Response
-    * Code 200
-        * **id**: Notebook id
-        * **description**: Notebook description.
-        * **creation_date**: Notebook creation data.
-        * **modification_date**: Notebook modification data.
-        * **annotations**: List of annotation id that was related.
-    * Code 404
-        * **detail**: Error detail.
-        * **loc**: Specifies which field the error is in.
-        * **msg**: Error message.
-        * **type**: Error type.
-    """
     return await notebook.create(body)
 
 
@@ -53,134 +32,34 @@ async def list(
         page: Optional[int] = None,
         size: Optional[int] = None
 ) -> Page[NotebookResponse]:
-    """
-    ## Description
-    * List all notebooks with pagination.
-
-    ## Parameters
-    * **page**: Page that you want to get. Defaults to 1.
-    * **size**: Size of the page that you want to get. Defaults to 1.
-
-    ## Response
-    * Code 200
-        * **id**: Notebook id
-        * **description**: Notebook description.
-        * **creation_date**: Notebook creation data.
-        * **modification_date**: Notebook modification data.
-        * **annotations**: List of annotation id that was related.
-    * Code 404
-        * **detail**: Error detail.
-        * **loc**: Specifies which field the error is in.
-        * **msg**: Error message.
-        * **type**: Error type.
-    """
     return await notebook.list(response)
 
 
-@router.get('/{notebook_id}', response_model=NotebookResponse)
+@router.get('/{annotation_id}')
 async def get(
-        notebook_id: str
+        annotation_id: str
 ) -> NotebookResponse:
-    """
-    ## Description
-    * List notebook detail.
-
-    ## Parameters
-    * **notebook_id**: Id of notebook that you need to put.
-
-    ## Response
-    * Code 200
-        * **id**: Notebook id
-        * **description**: Notebook description.
-        * **creation_date**: Notebook creation data.
-        * **modification_date**: Notebook modification data.
-        * **annotations**: List of annotation id that was related.
-    * Code 404
-        * **detail**: Error detail.
-        * **loc**: Specifies which field the error is in.
-        * **msg**: Error message.
-        * **type**: Error type.
-    """
-    return await notebook.get(notebook_id)
+    return await notebook.get(annotation_id)
 
 
-@router.put('/{notebook_id}', response_model=NotebookResponse)
+@router.put('/{annotation_id}')
 async def put(
-        notebook_id: str,
+        annotation_id: str,
         body: CreateAndPutNotebookRequest
 ) -> NotebookResponse:
-    """
-    ## Description
-    * Update all notebook fields.
-
-    ## Parameters
-    * **notebook_id**: Id of notebook that you need to put.
-
-    ## Body
-    * **description**: Description of your annotation.
-    * **annotations**: List of annotation ID that will be related.
-
-    ## Response
-    * Code 200
-        * **id**: Notebook id
-        * **description**: Notebook description.
-        * **creation_date**: Notebook creation data.
-        * **modification_date**: Notebook modification data.
-        * **annotations**: List of annotation id that was related.
-    * Code 404
-        * **detail**: Error detail.
-        * **loc**: Specifies which field the error is in.
-        * **msg**: Error message.
-        * **type**: Error type.
-    """
-    return await notebook.put(body, notebook_id)
+    return await notebook.put(body, annotation_id)
 
 
-@router.patch('/{notebook_id}', response_model=NotebookResponse)
+@router.patch('/{annotation_id}')
 async def patch(
-        notebook_id: str,
+        annotation_id: str,
         body: PatchNotebookRequest
 ) -> NotebookResponse:
-    """
-    ## Description
-    * Update notebook with optional fields.
-
-    ## Parameters
-    * **notebook_id**: Id of notebook that you need to patch.
-
-    ## Body
-    * **description**: Description of your annotation.
-    * **annotations**: List of annotation ID that will be related.
-
-    ## Response
-    * Code 200
-        * **id**: Notebook id
-        * **description**: Notebook description.
-        * **creation_date**: Notebook creation data.
-        * **modification_date**: Notebook modification data.
-        * **annotations**: List of annotation id that was related.
-    * Code 404
-        * **detail**: Error detail.
-        * **loc**: Specifies which field the error is in.
-        * **msg**: Error message.
-        * **type**: Error type.
-    """
-    return await notebook.patch(body, notebook_id)
+    return await notebook.patch(body, annotation_id)
 
 
-@router.delete(
-    '/{notebook_id}',
-    status_code=status.HTTP_204_NO_CONTENT,
-    response_description="No content"
-)
+@router.delete('/{annotation_id}')
 async def delete(
-        notebook_id: str
-):
-    """
-    ## Description
-    * Delete notebook.
-
-    ## Parameters
-    * **notebook_id**: Id of annotation that you need to delete.
-    """
-    await notebook.delete(notebook_id)
+        annotation_id: str
+) -> Response:
+    return await notebook.delete(annotation_id)
